0f8658958671d4e388829ae37d3c08611d54db50
Stephanie <swongte@hotmail.com>
2015-06-29 21:50:13 -0700

    hiding task in progress part 2

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -5,32 +5,41 @@ blocitoff.controller('Home.controller', ['$scope', '$firebaseArray', function($s
   //$scope.subText = "Welcome to the home page of Blocitoff";
 
   var ref = new Firebase("https://shining-inferno-4672.firebaseio.com/");
-
+  
+  //creating a syncronized array with firebase
   $scope.messages = $firebaseArray(ref);
 
           //ADD MESSAGE METHOD
           $scope.addMessage = function(e) {
+
           	
             //LISTEN FOR RETURN KEY
             if (e.keyCode === 13 && $scope.msg) {
-              //ALLOW CUSTOM OR ANONYMOUS USER NAMES
-              //var name = $scope.name || "anonymous";
+              
+              var taskDate = new Date();
+              $scope.taskDate = taskDate.getTime();
+              //var value = taskDate.getTime();
+              console.log($scope.taskDate);
 
               //ADD TO FIREBASE
               $scope.messages.$add({
-                //from: name,
+                date: $scope.taskDate,
+                //timestamp: Firebase.ServerValue.TIMESTAMP,
                 body: $scope.msg
               });
 
               //RESET MESSAGE
               $scope.msg = "";
-              
+
+    
+
             }
           }
 
    
    /*
-   $scope.taskDate = function(task_date){
+   $scope.dateCheck = function(value){
+   	 
    	 //pass in the date of when task was written
      
      //if task date is older than 7 days
@@ -39,13 +48,13 @@ blocitoff.controller('Home.controller', ['$scope', '$firebaseArray', function($s
      //else task date is not older than 7
      //days and function returns true
 
-     if (task_date == 'hello'){
-     	return false;
-     else
+     if (value % 2 == 0){
      	return true;
-     }
+     else
+     	return false;
+     
 
-   } */
+   }*/
   
 
 }]);
